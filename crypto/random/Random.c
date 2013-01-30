14a5338658b3121d3559fd9233b5bed51e455664
Caleb James DeLisle <calebdelisle@lavabit.com>
2013-01-30 12:37:04 -0500

    Improved description of Random's security properties.

--- crypto/random/Random.c
+++ crypto/random/Random.c
@@ -24,14 +24,14 @@
 #include <crypto_stream_salsa20.h>
 
 /**
- * cjdns random generator.
+ * cjdns random generator:
  * It is with great apprehension that I have decided to go forward with this random generator.
  * Sadly there doesn't exist any plain-and-simple random generation library for C without
- * bundling libevent or openssl or some other megalyth.
+ * bundling libevent, openssl or some other megalyth.
  *
  * Additionally most random generators use a feedback loop which is difficult to validate as
  * it has a period which is not immedietly obvious by looking at it. Additionally, this
- * feedbck loop design leads to issues like:
+ * feedback loop design leads to issues like:
  * http://www.openssl.org/news/secadv_prng.txt
  *
  * How this random generator works:
@@ -46,17 +46,45 @@
  *
  * Adding entropy to the generator is as follows:
  * Random_addRandom() adds a sample of randomness by rotating and XORing it into
- * Random_SeedGen.collectedEntropy, critical to the design is that Random_SeedGen.seed
- * is never touched. This means even if Random_addRandom() is so deeply flawed that it causes
- * collectedEntropy to become predictable, there is still 256 bits of primary random material
- * from the operating system in Random_SeedGen.seed which is never touched as long as the
- * application runs.
- *
- * Every 256 calls to Random_addRandom(), Random_SeedGen is again hashed into random.tempSeed.
- * Since collectedEntropy is a 256 bit buffer, each call should be worth at least 1 bit.
+ * Random_SeedGen.collectedEntropy.
+ * Every 256 calls to Random_addRandom(), Random_SeedGen is again hashed into Random.tempSeed.
  * Note that Random.nonce is *not* reset ever during the operation of the generator because
- * otherwise, 512 successive calls to Random_addRandom() with the same input would lead to
- * the random generator duplicating it's output.
+ * otherwise, 512 successive calls to Random_addRandom() with the same input would cause the
+ * random generator to repeat.
+ *
+ *
+ * State-compromize extension:
+ * It is acknoliged that a compromize of the generator's internal state will result in the
+ * attacker knowing every output which has been and will be generated or with the current
+ * tempSeed. After a further 256 calls to Random_addRandom(), the generator should recover.
+ *
+ * While using a feedback loop with a one-way hash function to frustrate backtracking seems
+ * enticing, it stands to reason that the only way a hash function can be one-way is by
+ * destroying entropy, destruction of entropy in a feedback system could lead to an oscillation
+ * effect when it becomes entropy starved. Though this issue does not seem to have been
+ * exploited in other prngs, proving that it cannot be exploited is beyond my abilities and the
+ * devil you know is better than the devil you don't.
+ *
+ *
+ * Iterative Guessing:
+ * This generator only introduces the entropy given by Random_addRandom() once every 256 calls.
+ * Assuming each call introduces at least 1 bit of good entropy, iterative guessing requires
+ * guessing a 256 bit value for each iteration.
+ *
+ *
+ * Input based Attacks:
+ * The generator is as conservitive as possible about the entropy provided in calls to
+ * Random_addRandom(), valuing each at 1 bit of entropy. Since the number is rotated and XORd
+ * into collectedEntropy, some calls with 0 bits of entropy can be smoothed over by other calls
+ * with > 1 bit of entropy. If Random_addRandom() is called arbitrarily many times with 0 bits
+ * of entropy, since the inputs are XORd into collectedEntropy the entropy level of
+ * collectedEntropy will remain unchanged.
+ *
+ * Even if the attacker is able to gather information from the generator's output and craft
+ * inputs to Random_addRandom() which *decrease* the entropy in collectedEntropy, this will not
+ * decrease the performance of the generator itself because the 256 bit Random_SeedGen.seed
+ * is seeded with the primary seed meterial (eg dev/urandom) and never altered for duration of
+ * the generator's operation.
  */
 
 /** How many bytes to buffer so requests for a small amount of random do not invoke salsa20. */
